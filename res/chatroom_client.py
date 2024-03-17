f1dc1fee10cac492c6340af556110f60dc0ab2a5
MicroFIsh <2609948707@qq.com>
2024-03-17 17:05:36 +0800

    优化了代码结构，增加了字符串，丰富的注释

--- res/chatroom_client.py
+++ res/chatroom_client.py
@@ -1,40 +1,46 @@
 # By ViudiraTech
 # GPL-2.0 LICENSE ¯\_(ツ)_/¯
 
-import socket
-import threading
-import tkinter as tk
-from tkinter import messagebox
-import ctypes
-import shelve
-import pickle
-import os
-import appdirs  # 需要安装这个库来获取应用程序数据目录
-import time
+# 导入必要的库
+import socket  # 用于创建网络通信的socket
+import threading  # 用于创建和管理线程
+import tkinter as tk  # 用于创建GUI
+from tkinter import messagebox  # 用于显示消息框
+import ctypes  # 用于调用Windows API函数
+import shelve  # 用于创建简单的持久化数据库
+import pickle  # 用于序列化和反序列化Python对象
+import os  # 用于操作系统功能，如文件路径操作
+import appdirs  # 用于获取应用程序数据目录的库
+import time  # 用于时间相关的功能
 
 # 全局变量用于存储socket客户端对象和连接状态
 client = None
 connected = False
 login_window = None
 
+# 登录界面的GUI类
 class LoginGUI:
     def __init__(self, master):
-        self.master = master  # 存储传入的 Tk 实例
-        self.master.title("Paizer客户端-登录")
-        self.master.geometry("280x150")  # 调整登录窗口大小
-        # 设置窗口的最小和最大尺寸为当前大小，从而禁止调整窗口大小
-        self.master.resizable(width=False, height=False)
+        # 初始化方法，设置窗口属性和控件
+        self.master = master  # 存储传入的Tk实例
+        self.master.title("Paizer客户端 - 登录")  # 设置窗口标题
+        self.master.geometry("280x150")  # 设置登录窗口大小
+        self.master.resizable(width=False, height=False)  # 禁止调整窗口大小
+
         # 昵称标签和输入框
-        self.nickname_label = tk.Label(self.master, text="键入您的昵称（支持中、英文和数字）:")
+        self.nickname_label = tk.Label(self.master, text="用户昵称(支持中英文、数字符号):")
         self.nickname_label.pack()
         self.nickname_entry = tk.Entry(self.master, width=20)
         self.nickname_entry.pack()
 
         # 服务器IP地址标签和输入框
-        self.server_ip_port_label = tk.Label(self.master, text="键入服务器IP地址(可选端口，默认21156):")
+        self.server_ip_port_label = tk.Label(self.master, text="服务器IP地址(端口留空为21156):")
         self.server_ip_port_label.pack()
         self.server_ip_port_entry = tk.Entry(self.master, width=20)
         self.server_ip_port_entry.pack()
+
+        # 绑定回车键事件到 login_on_enter 方法
+        self.master.bind('<KeyPress-Return>', self.login_on_enter)
 
         # 登录按钮
         self.login_button = tk.Button(self.master, text="登录服务器", command=self.login, width=15)
@@ -45,28 +51,35 @@ class LoginGUI:
         self.nickname_entry.insert(0, self.nickname)
         self.server_ip_port_entry.insert(0, self.server_ip_port)
 
+    def login_on_enter(self, event):
+        # 调用登录方法，忽略事件对象
+        self.login()
+
     def login(self):
+        # 获取输入的昵称和服务器IP端口
         nickname = self.nickname_entry.get()
         server_ip_port = self.server_ip_port_entry.get()
 
         # 保存输入数据
-        save_input_data(self.nickname_entry.get(), self.server_ip_port_entry.get())
+        save_input_data(nickname, server_ip_port)
 
         # 验证输入
         if validate_input(nickname, server_ip_port):
-            # 如果验证通过，调用 on_connect 函数
+            # 如果验证通过，调用on_connect函数
             global login_window
             login_window = self.master  # 确保全局变量引用当前的登录窗口
             on_connect(nickname, server_ip_port, self.master)
-            
+
             # 隐藏登录窗口
             self.master.withdraw()
 
+# 客户端GUI的主类
 class PaizerClientGUI:
     def __init__(self, master, nickname, server_ip_port):
+        # 初始化方法，设置窗口属性、控件和事件处理
         self.master = master
-        self.master.title("Paizer客户端-主页")
-        self.master.geometry("570x470")  # 调整GUI窗口大小
+        self.master.title(f"Paizer客户端 - {server_ip_port} - {nickname}")  # 设置窗口标题
+        self.master.geometry("570x470")  # 设置GUI窗口大小
         self.master.resizable(width=False, height=False)
 
         self.nickname = nickname
@@ -88,7 +101,7 @@ class PaizerClientGUI:
         bottom_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
 
         # 输入框
-        self.message_entry = tk.Entry(bottom_frame, width=50)
+        self.message_entry = tk.Entry(bottom_frame, width=60)
         self.message_entry.pack(side=tk.LEFT, fill=tk.X, padx=(10, 5), pady=(10, 10), expand=True)
 
         # 绑定回车键事件
@@ -100,7 +113,7 @@ class PaizerClientGUI:
 
         # 尝试连接到服务器
         self.create_connection()
-        
+
         self.master.protocol("WM_DELETE_WINDOW", self.on_close)
 
     def on_close(self):
@@ -152,6 +165,7 @@ class PaizerClientGUI:
                 connected = False
 
     def send_message(self):
+        # 发送消息到服务器的方法
         message = self.message_entry.get()
         if message:
             # 格式化消息为 "用户名: 消息内容"
@@ -160,6 +174,7 @@ class PaizerClientGUI:
             self.message_entry.delete(0, tk.END)
 
     def receive_message(self):
+        # 接收来自服务器的消息的方法
         while True:
             try:
                 message = client.recv(1024).decode('utf-8')
@@ -173,12 +188,13 @@ class PaizerClientGUI:
                 break
 
     def log_message(self, message):
-        # 将消息添加到文本框中
+        # 将消息添加到文本框中的方法
         self.message_text.config(state='normal')  # 允许编辑文本框
         self.message_text.insert(tk.END, message + '\n')
         self.message_text.config(state='disabled')  # 禁止编辑文本框
         self.message_text.yview(tk.END)  # 自动滚动到最新消息
 
+# 保存用户输入数据到本地的方法
 def save_input_data(nickname, server_ip_port):
     # 获取应用程序数据目录
     data_dir = appdirs.user_data_dir('Paizer', 'ViudiraTech')
@@ -190,30 +206,31 @@ def save_input_data(nickname, server_ip_port):
             db['server_ip_port'] = server_ip_port
     except Exception as e:
         print(f"[!] 保存输入数据时发生错误: {e}")
-        
+
+# 从本地加载用户输入数据的方法
 def load_input_data():
     # 获取应用程序数据目录
     data_dir = appdirs.user_data_dir('Paizer', 'ViudiraTech')
     # 创建配置文件的完整路径
     config_file = os.path.join(data_dir, 'user_data.dat')
-    
+
     # 确保数据目录存在
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
-    
+
     # 尝试加载或创建数据
     try:
-        with shelve.open(config_file, 'c') as db:  # 使用'c'模式创建新数据库
-            nickname = db.get('nickname', '')  # 默认昵称
-            server_ip_port = db.get('server_ip_port', '')  # 默认服务器IP和端口
-            db['nickname'] = nickname  # 保存默认值
-            db['server_ip_port'] = server_ip_port  # 保存默认值
+        with shelve.open(config_file, 'r') as db:  # 使用'r'模式打开现有数据库
+            nickname = db.get('nickname', '')  # 获取保存的昵称，如果不存在则返回空字符串
+            server_ip_port = db.get('server_ip_port', '')  # 获取保存的服务器IP和端口，如果不存在则返回空字符串
             return nickname, server_ip_port
     except Exception as e:
         print(f"[!] 加载输入数据时发生错误: {e}")
         return '', ''  # 如果发生错误，返回默认值
 
+# 验证用户输入是否有效的方法
 def validate_input(nickname, server_ip_port, parent=None):
+    # 检查昵称是否为空
     if not nickname.strip():
         messagebox.showwarning("警告！", "昵称不能为空。", parent=parent)
         return False
@@ -236,21 +253,20 @@ def validate_input(nickname, server_ip_port, parent=None):
         return False
     return True
 
+# 当连接到服务器后创建新的GUI窗口的方法
 def on_connect(nickname, server_ip_port, login_root):
     # 隐藏登录窗口
     login_root.withdraw()
 
-    # 创建新的 GUI 窗口实例
+    # 创建新的GUI窗口实例
     root = tk.Tk()
-    root.title("Paizer客户端")
     app = PaizerClientGUI(root, nickname, server_ip_port)
-    
-    # 启动新的 GUI 窗口的主循环
+
+    # 启动新的GUI窗口的主循环
     root.mainloop()
 
-
+# 主函数，程序入口点
 def main():
-    # ctypes.windll.kernel32.FreeConsole()
     # 创建登录窗口
     login_root = tk.Tk()
     global login_window
@@ -258,5 +274,6 @@ def main():
     login_app = LoginGUI(login_root)
     login_root.mainloop()
 
+# 程序运行时的入口点
 if __name__ == "__main__":
     main()
--- res/chatroom_server.py
+++ res/chatroom_server.py
@@ -1,94 +1,98 @@
 # By ViudiraTech
 # GPL-2.0 LICENSE ¯\_(ツ)_/¯
 
-import socket
-import threading
-from datetime import datetime
+# 导入必要的库
+import socket  # 用于网络通信
+import threading  # 用于创建和管理线程
+from datetime import datetime  # 用于获取当前时间
 
+# 获取本机的IPv4地址
 def get_local_ipv4():
-    interfaces = socket.getaddrinfo(socket.gethostname(), None)
+    interfaces = socket.getaddrinfo(socket.gethostname(), None)  # 获取主机名对应的IP地址信息
     for interface in interfaces:
-        address = interface[4][0]
+        address = interface[4][0]  # 获取IP地址
         if address.startswith('192.168') or address.startswith('10.') or address.startswith('172.'):
-            return address
-    return '无法获取IPv4内网地址'
+            return address  # 如果地址是私有地址，则返回
+    return '无法获取IPv4内网地址'  # 如果无法获取有效的内网地址，则返回错误信息
 
+# 获取服务端监听端口
 def get_server_port():
     while True:
-        input_port = input("设置服务端监听端口（留空为21156）：")
-        if not input_port:
+        input_port = input("设置服务端监听端口（留空为21156）：")  # 提示用户输入端口号
+        if not input_port:  # 如果用户没有输入，则使用默认端口21156
             port = 21156
         else:
             try:
-                port = int(input_port)
-                if 0 < port < 65536:
+                port = int(input_port)  # 尝试将输入转换为整数
+                if 0 < port < 65536:  # 检查端口号是否在有效范围内
                     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as test_socket:
                         try:
-                            test_socket.bind(('localhost', port))
-                        except PermissionError:
+                            test_socket.bind(('localhost', port))  # 尝试绑定端口
+                        except PermissionError:  # 如果端口已被占用或不允许访问
                             print(f"端口 {port} 已被占用或不允许访问。请重新输入端口号。")
                             continue
                 else:
                     print("端口号无效，请输入1到65535之间的数字。")
-            except ValueError:
+            except ValueError:  # 如果输入不是有效的整数
                 print("请输入一个有效的端口号。")
-        return port
+        return port  # 返回用户输入的有效端口号
 
+# 初始化服务端
 print('Paizer服务端\n')
-
-server_port = get_server_port()
+server_port = get_server_port()  # 获取并设置服务端监听端口
 print()
-print(f"[*] 当前服务端监听端口: {server_port}")
+print(f"[*] 当前服务端监听端口: {server_port}")  # 打印服务端监听的端口
 
-local_ipv4 = get_local_ipv4()
-print(f"[*] 当前服务器内网地址: {local_ipv4}")
+local_ipv4 = get_local_ipv4()  # 获取本机内网IP地址
+print(f"[*] 当前服务器内网地址: {local_ipv4}")  # 打印内网IP地址
 
+# 创建socket对象并绑定端口，开始监听
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', server_port))
 server.listen(5)
-server_ip, server_port = server.getsockname()
-print(f"[*] 正在监听 {server_ip}:{server_port}\n")
+server_ip, server_port = server.getsockname()  # 获取绑定的IP地址和端口号
+print(f"[*] 正在监听 {server_ip}:{server_port}\n")  # 打印服务器监听的信息
 
-clients = []
+clients = []  # 存储所有客户端的列表
 
+# 处理客户端连接的函数
 def handle_client(client_socket, client_address, broadcast):
     # 接收客户端的昵称
     client_nickname = client_socket.recv(1024).decode('utf-8').strip(': ')
     current_time = datetime.now().strftime("%H:%M:%S")
-    print(f"\n[*] [{current_time}] {client_nickname} 加入了服务器\n")
-    broadcast(client_socket, f"\n[*] [{current_time}] {client_nickname} 加入了服务器\n")
+    print(f"\n[*] [{current_time}] {client_nickname} 加入了服务器\n")  # 打印客户端加入信息
+    broadcast(client_socket, f"\n[*] [{current_time}] {client_nickname} 加入了服务器\n")  # 广播客户端加入信息
     
     while True:
         try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message == "CLOSE_REQUEST":
-                # 客户端发送了关闭请求
-                close_client_socket(clients, broadcast, client_socket, client_nickname)
-                break  # 退出循环，因为客户端已经断开
+            message = client_socket.recv(1024).decode('utf-8')  # 接收客户端发送的消息
+            if message == "CLOSE_REQUEST":  # 如果消息是关闭请求
+                close_client_socket(clients, broadcast, client_socket, client_nickname)  # 关闭客户端连接
+                break  # 退出循环
             elif message:
-                # 假设消息格式为 "用户名: 消息内容"
+                # 处理接收到的消息
                 parts = message.split(":", 1)  # 分割用户名和消息内容
                 if len(parts) == 2:
                     username, message_content = parts
                     current_time = datetime.now().strftime("%H:%M:%S")
-                    # 格式化消息，包含时间戳和消息内容
+                    # 格式化消息，并打印
                     formatted_message = f"[{current_time}] {username}: {message_content}"
                     print(formatted_message)
-                    # 广播消息给所有连接的客户端，包括发送者
+                    # 广播消息给所有连接的客户端
                     broadcast(None, formatted_message)
                 else:
                     # 如果消息格式不正确，发送错误消息
                     client_socket.send("错误的格式，消息未发送。".encode('utf-8'))
         except Exception as e:
-            print(f"[!] Exception: {e}")
+            print(f"[!] Exception: {e}")  # 打印异常信息
             # 从clients列表中移除异常的客户端
             clients.remove(client_socket)
             client_socket.close()
             current_time = datetime.now().strftime("%H:%M:%S")
-            print(f"\n[*] [{current_time}] {client_nickname} 因异常断开连接\n")
-            broadcast(client_socket, f"\n[*] [{current_time}] {client_nickname} 因异常断开连接\n")  # 广播断开连接消息
+            print(f"\n[*] [{current_time}] {client_nickname} 因异常断开连接\n")  # 打印客户端异常断开信息
+            broadcast(client_socket, f"\n[*] [{current_time}] {client_nickname} 因异常断开连接\n")  # 广播客户端异常断开信息
             break
-            
+
 # 全局函数，用于关闭客户端连接并广播断开连接消息
 def close_client_socket(clients, broadcast, client_socket, client_nickname):
     try:
@@ -107,18 +111,19 @@ def close_client_socket(clients, broadcast, client_socket, client_nickname):
     except Exception as e:
         print(f"[!] 关闭客户端连接时发生错误: {e}")
 
+# 广播消息给所有客户端，除了target
 def broadcast(target, message):
-    # 广播消息给所有客户端，除了target
     for client in clients:
         if client != target:
             try:
-                client.send(message.encode('utf-8'))
+                client.send(message.encode('utf-8'))  # 发送消息给客户端
             except Exception as e:
                 print(f"[!] 无法发送消息到 {client.getpeername()}: {e}")
 
+# 主循环，接受客户端连接
 while True:
-    client, address = server.accept()
-    clients.append(client)
+    client, address = server.accept()  # 接受新的客户端连接
+    clients.append(client)  # 将新的客户端添加到客户端列表中
 
-    client_thread = threading.Thread(target=handle_client, args=(client, address, broadcast))
-    client_thread.start()
+    client_thread = threading.Thread(target=handle_client, args=(client, address, broadcast))  # 创建处理客户端的新线程
+    client_thread.start()  # 启动线程
